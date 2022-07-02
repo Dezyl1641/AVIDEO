b843b2049fd7624f81257d238bed26e77af8d38a
Dezyl1641 <dezylsolanki3@gmail.com>
2022-07-02 14:57:06 +0530

    d

--- app.js
+++ app.js
@@ -29,47 +29,47 @@ const router = require("express").Router();
 const keys = require('./config/keys')
 const passportSetup = require('./config/passport-setup')
 
-// const passport = require('passport');
-// app.use(passport.initialize());
-// app.use(passport.session());
+const passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session());
 
 
-// var username = "initialUsername";
+var username = "initialUsername";
 
 
-// const authCheck = (req, res, next) => {
-//     // console.log(username);
-//     // next();
-//     if(username === "initialUsername"){
-//         res.render('404');
-//     } else {
-//         next();
-//     }
-//     next();
-// };
+const authCheck = (req, res, next) => {
+    // console.log(username);
+    // next();
+    if(username === "initialUsername"){
+        res.render('404');
+    } else {
+        next();
+    }
+    next();
+};
 
-// app.get('/home', authCheck, (req,res)=>{
-//     // res.render('home', {user: username});
-//     res.render('test');
-// })
+app.get('/home', authCheck, (req,res)=>{
+    // res.render('home', {user: username});
+    res.render('test');
+})
 
-// app.get("/room", authCheck, function(req, res){
-//     res.redirect(`/${uuidV4()}`);
-//     // res.redirect("/uuidV4()");
-// });
+app.get("/room", authCheck, function(req, res){
+    res.redirect(`/${uuidV4()}`);
+    // res.redirect("/uuidV4()");
+});
 
-// var email;
-// var url = require('url');
+var email;
+var url = require('url');
 
-// function fullUrl(req) {
-//   return url.format({
-//     protocol: req.protocol,
-//     host: req.get('host'),
-//     pathname: req.originalUrl
-//   });
-// }
+function fullUrl(req) {
+  return url.format({
+    protocol: req.protocol,
+    host: req.get('host'),
+    pathname: req.originalUrl
+  });
+}
 
-// var link;
+var link;
 app.get('/', (req,res)=>{
     // link = fullUrl(req);
     // console.log(link);
@@ -79,83 +79,83 @@ app.get('/', (req,res)=>{
     // res.render('test');
 })
 
-// app.get('/auth/login', (req,res)=>{
-//     res.render('login');
-// })
+app.get('/auth/login', (req,res)=>{
+    res.render('login');
+})
 
-// app.get('/auth/google', passport.authenticate('google', {
-//     scope: ['profile'],
-//     // prompt: 'select_account'
-// }));
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile'],
+    // prompt: 'select_account'
+}));
 
 
-// app.get('/auth/logout', (req,res,next)=>{
-//     // req.session.destroy();
-//     console.log("hue");
-//     req.logout(function(err) {
+app.get('/auth/logout', (req,res,next)=>{
+    // req.session.destroy();
+    console.log("hue");
+    req.logout(function(err) {
         
-//         if (err) { return next(err); }
-//         res.redirect('/');
-//     });
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
     
-// })
-// app.get('/auth/logout', (req,res,next)=>{
-//     // req.session.destroy();
-//     console.log("hue");
-//     req.logout(function(err) {
+})
+app.get('/auth/logout', (req,res,next)=>{
+    // req.session.destroy();
+    console.log("hue");
+    req.logout(function(err) {
         
-//         if (err) { return next(err); }
-//         res.redirect('/');
-//     });
-//     console.log(username);
-// })
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
+    console.log(username);
+})
 
-// app.get('/auth/google/redirect', passport.authenticate('google'), (req,res)=>{
-//     username = req.user.username;
-//     res.redirect('/home');
-// })
-
-
-// var ROOM_ID;
-// app.get("/:room", authCheck,  function(req, res){
-//     var temp =  req.params.room;
-//     if(temp.length > 12) ROOM_ID = req.params.room;
-//     // console.log(ROOM_ID);
-//     res.render("room", {roomId: req.params.room, user: username});
-// });
+app.get('/auth/google/redirect', passport.authenticate('google'), (req,res)=>{
+    username = req.user.username;
+    res.redirect('/home');
+})
 
 
+var ROOM_ID;
+app.get("/:room", authCheck,  function(req, res){
+    var temp =  req.params.room;
+    if(temp.length > 12) ROOM_ID = req.params.room;
+    // console.log(ROOM_ID);
+    res.render("room", {roomId: req.params.room, user: username});
+});
 
-// app.post('/room', (req, res) => {
-//     console.log("hue");
-//     console.log(req.body);
-//     email = req.body.email;
-//     const transporter = nodemailer.createTransport({
-//         service: 'gmail',
-//         auth: {
-//             user: keys.credentials.email,
-//             pass: keys.credentials.password
-//         }
-//     })
 
-//     const mailOptions = {
-//         from: 'dezylsolanki3@gmail.com',
-//         to: email,
-//         subject: 'Meet reminder!',
-//         text: `Your meet with ${username} is about to start. The Room ID is ${link}${ROOM_ID}`
-//     }
 
-//     transporter.sendMail(mailOptions, (error, info) => {
-//         if(error){
-//             console.log(email);
-//             console.log(error);
-//         }
-//         else{
-//             console.log('success');
-//             alert('Email sent!');
-//         }
-//     })
-// })
+app.post('/room', (req, res) => {
+    console.log("hue");
+    console.log(req.body);
+    email = req.body.email;
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: keys.credentials.email,
+            pass: keys.credentials.password
+        }
+    })
+
+    const mailOptions = {
+        from: 'dezylsolanki3@gmail.com',
+        to: email,
+        subject: 'Meet reminder!',
+        text: `Your meet with ${username} is about to start. The Room ID is ${link}${ROOM_ID}`
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if(error){
+            console.log(email);
+            console.log(error);
+        }
+        else{
+            console.log('success');
+            alert('Email sent!');
+        }
+    })
+})
 
 
 // io.sockets.on("connection", (socket) => {
